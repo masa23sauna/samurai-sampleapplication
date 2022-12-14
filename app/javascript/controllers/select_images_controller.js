5fa20007b8d6486bb8b909c7ca870d9b4f2c40f7
“masa23sauna” <m.konno2523@outlook.com>
2022-12-14 10:54:47 +0000

    画像プレビューの調整(途中経過)

---
+++ app/javascript/controllers/select_images_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "file", "image" ]
+  connect() {
+  }
+  fileCheck() {
+    // fileTargetが変更されたタイミングで処理を開始
+    this.fileTarget.addEventListener('change', e => {
+      const preview = this.imageTarget
+      const file = e.target.files[0]
+      // FileReaderのインスタンスを作成
+      const reader = new FileReader()
+
+      // readerがファイルの読み込みができたら処理を開始
+      reader.addEventListener("load", () => {
+      //画像ファイルを base64 文字列に変換してimgタグに代入
+        preview.src = reader.result
+      }, false)
+      // ファイルの読み込みを実行
+      reader.readAsDataURL(file)
+    })
+  }
+}
